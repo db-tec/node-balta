0e79075256dabe6bb6283ebd460031e45d74456f
db-tec <eng.davidborges@gmail.com>
2020-02-20 17:24:51 -0300

    metodo delete

--- src/app.js
+++ src/app.js
@@ -8,20 +8,33 @@ const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next)=> {
+const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API",
-        version: "0.0.1"
+        version: "0.0.2"
     });
-
 });
 
-const create = router.post('/', (req, res, next)=> {
-    res.status(201 ).send(req.body);
+// Metodo create 201
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
 
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
 });
 
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
